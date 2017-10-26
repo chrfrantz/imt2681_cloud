88800efb71927057dc5aa07721ca52d75ad3376e
Mariusz Nowostawski <nowostawski@gmail.com>
2017-10-26 06:33:37 +0200

    Changing the time delay for notifications to 10min.

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -32,7 +32,7 @@ func sendDiscordLogEntry(what string) {
 func main() {
 	for {
 		text := "Heroku timer test at: " + time.Now().String()
-		delay := time.Minute * 30
+		delay := time.Minute * 10
 
 		sendDiscordLogEntry(text)
 		time.Sleep(delay)
